462a8a8f11e8c1cc419a9279fa977061b8fc1276
Simo Råman <simoraman@gmail.com>
2014-11-23 21:07:50 +0200

    all keys in template should be found in json under test

--- src/jekker/core.clj
+++ src/jekker/core.clj
@@ -1,7 +1,13 @@
 (ns jekker.core
   (:require [cheshire.core :refer :all]))
 
+(defn in?
+  "true if seq contains elm"
+  [seq elm]
+  (some #(= elm %) seq))
+
 (defn check [template-string json-string]
   (let [template (parse-string template-string true)
-        json (parse-string json-string true)]
-    (map = (keys template) (keys json))))
+        json (parse-string json-string true)
+        key-matches (map #(in? (keys json) %1) (keys template))]
+    (not (or (in? key-matches false) (in? key-matches nil)))))
--- test/jekker/core_test.clj
+++ test/jekker/core_test.clj
@@ -2,11 +2,20 @@
   (:require [clojure.test :refer :all]
             [jekker.core :refer :all]))
 
-(def template "{\"key\":\"val\"}")
-(def json "{\"key\":\"some value\"}")
-
 (deftest matching-keys
   (testing "json keys should match template"
-    (is (check template json))))
+    (let [template "{\"key\":\"val\"}"
+          json "{\"key\":\"some value\"}"]
+      (is (check template json)))))
 
-(deftest test (is '(true false)))
+(deftest non-matching-keys
+  (testing "non-matching keys should fail"
+    (let [template "{\"key\":\"val\"}"
+          json "{\"foo\":\"some value\"}"]
+      (is (not (check template json))))))
+
+(deftest order
+  (testing "order should not matter"
+    (let [template "{\"key\":\"val\", \"foo\":\"bar\"}"
+          json "{\"foo\":\"some value\", \"key\":\"val\"}"]
+      (is (check template json)))))
