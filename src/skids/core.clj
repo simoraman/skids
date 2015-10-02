0aef0a664c77a0780a79887289aeefc8ecd56773
Simo Råman <simoraman@gmail.com>
2015-10-02 23:26:21 +0300

    check keys

--- src/skids/core.clj
+++ src/skids/core.clj
@@ -15,47 +15,11 @@
 
 (declare matches)
 
-;;TODO should check all values in the target json
-(defn check-array [matcher val]
-  (if (vector? val)
-    (if (map? (first val))
-      (every? (fn [x] (true? (:valid x))) (map (partial matches (first matcher)) val))
-      true)
-    false))
-
-(defn- value-match? [matcher val]
-  (cond
-   (boolean? matcher) (boolean? val)
-   (vector? matcher) (check-array matcher val)
-   (number? matcher) (number? val)
-   (string? matcher) (string? val)
-   (map? matcher) (if (map? val) (matches matcher val) false)
-   :else true))
-
-(defn true-maybe? [x]
-  (or (true? x) (true? (:valid x))))
-(defn values-match? [template json]
-  (let [resp (every? true-maybe? (map (fn [x]
-                                        (value-match? (get template (key x)) (val x)))
-                                json))]
-    (create-response resp)))
-
-(defn- key-match? [json key-to-test]
-  (cond
-   (in? (keys json) key-to-test) (create-response true)
-   :else (create-response false (str "key " (name key-to-test) " not found"))))
-
-(defn keys-match? [template json]
-  (first (filter (fn [x] (not (:valid x))) (map #(key-match? json %1) (keys template)))))
-
-(defn- matches [template json]
-  (let [keys-ok? (keys-match? template json)]
-    (cond
-     (empty? keys-ok?) (values-match? template json)
-     :else keys-ok?)))
+(defn matches [template json]
+  (let [has-keys? (map #(in? (keys json) %) (keys template))]
+    (every? true? has-keys?)))
 
 (defn check [template-string json-string]
   (let [template (parse-string template-string true)
-        json (parse-string json-string true)
-        m (matches template json)]
-    m))
+        json (parse-string json-string true)]
+    {:valid (matches template json)}))
